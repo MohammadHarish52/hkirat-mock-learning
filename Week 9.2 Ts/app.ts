58a667cba5bebd40bbfba376b0b7b9b77af07021
tachyonok <tachyonparticle123@gmail.com>
2024-09-29 10:34:32 +0530

    fucntion arguments questions checkpoint

--- Week 9.2 Ts/app.ts
+++ Week 9.2 Ts/app.ts
@@ -1,5 +1,37 @@
+//basic types
+// number. string , null ,undefined , boolean
+
 function print(firstName: String) {
   console.log(`hello ${firstName}`);
 }
 
 print("Harish");
+
+//2
+function sum(a: number, b: number): number {
+  return a + b;
+}
+console.log(sum(4, 5));
+
+//3
+function adult(age: number): boolean {
+  if (age > 18) {
+    return true;
+  }
+  return false;
+}
+
+console.log(adult(56));
+
+//4
+function two(fn: () => void) {
+  setTimeout(() => {
+    fn();
+  }, 1000);
+}
+
+function delayFn() {
+  console.log("hello ts");
+}
+
+two(delayFn);
