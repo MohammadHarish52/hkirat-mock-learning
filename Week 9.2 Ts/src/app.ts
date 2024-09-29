a84a8c32a2f348eadfd445928367f2c3a8d1da22
tachyonok <tachyonparticle123@gmail.com>
2024-09-29 20:30:53 +0530

    enum in ts

--- Week 9.2 Ts/src/app.ts
+++ Week 9.2 Ts/src/app.ts
@@ -1,31 +1,17 @@
-const user = {
-  firstName: "Harish",
-  lastName: "Mohammad",
-  age: 22,
-  occupation: "Developer",
-};
+// enum is used to define constants
 
-// main difference between types and interfaces is that interfaces lets u
-// implement classes while types lets u use union and intersections
-
-interface User {
-  firstName: string;
-  lastName: string;
-  age: number;
-  occupation: string;
+enum Direction {
+  up,
+  left,
+  down,
+  right,
 }
-type User2 = {
-  firstName: string;
-  lastName: string;
-  age: string | number;
-  occupation: string;
-};
 
-function checkAdult(user: User) {
-  if (user.age > 18) {
-    return true;
+function doSomething(keypress: Direction) {
+  if (keypress === Direction.up) {
   }
-  return false;
 }
 
-console.log(checkAdult(user));
+doSomething(Direction.left);
+doSomething(Direction.right);
+doSomething(Direction.up);
